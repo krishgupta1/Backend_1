311954e9d8fa9c5dfbd85cd1adb91b1173142cec
krishgupta1 <krishgupta11110@gmail.com>
2023-06-11 23:13:26 +0530

    Nothing

---
+++ server.js
@@ -0,0 +1,20 @@
+// Server Initiate
+const express = require('express');
+const app = express();
+
+// Activate the server on 8000 
+app.listen(8000, () => {
+    console.log("Server Started at port no. 8000");
+});
+
+app.get('/', (request, response) => { 
+    response.send("Hello ji, Kaise ho saare");
+})
+
+app.post('api/cars', (req, res) => {
+    const {name, brand} = request.body;
+    console.log(name);
+    console.log(brand);
+    console.log("Car submitted Successfully");
+})
+console.log("Nothing will added today")
